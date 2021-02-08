9cf773dc275aa440bdd0644f9fbef7aba81f24de
iliaSoto4ka <74914772+iliaSoto4ka@users.noreply.github.com>
2021-02-08 17:22:22 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -3,7 +3,17 @@ import sys
 
 import pygame
 import requests
-API_KEY = '40d1649f-0493-4b70-98ba-98533de7710b'
+
+
+def change_spn(sender):
+    global map_params
+    a = list(map(float, map_params['spn'].split(',')))[0]
+    if sender == pygame.K_PAGEUP:
+        a = str(a + 0.01)
+    else:
+        a = str(a - 0.01)
+    map_params['spn'] = f'{a},{a}'
+
 
 toponym_to_find = ','.join(input('Введите координаты в виде (координатаХ,координатаУ): ').split(',')[::-1])
 spn = input('Введите масштаб в виде (значение,значение): ')
@@ -33,5 +43,13 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_PAGEUP or event.key == pygame.K_PAGEDOWN:
+                change_spn(event.key)
+                response = requests.get(map_api_server, params=map_params)
+                map_file = "map.png"
+                with open(map_file, "wb") as file:
+                    file.write(response.content)
+                screen.blit(pygame.image.load(map_file), (0, 0))
 pygame.quit()
 os.remove(map_file)
